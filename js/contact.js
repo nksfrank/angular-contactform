7c031c4da961466773697a2fed541d91f3794bdd
nksfrank <niklas.frank@gmail.com>
2015-03-08 21:34:42 +0100

    fix: added resolve to post
    Pending Posts should now abort if a new one is sent

--- js/contact.js
+++ js/contact.js
@@ -1,26 +1,31 @@
 var contactApp = angular.module("contactApp", []);
 
-contactApp.controller("ContactCtrl", function($scope, $http) {
+contactApp.controller("ContactCtrl", function($scope, $http, $timeout, $q) {
     $scope.contact = {};
-    $scope.err = {};
 
-    $scope.Steps = [0, 1, 2];
-    $scope.currentStep = 0;
-
+    var contactRequest = null;
     $scope.contactSend = function() {
+        if(contactRequest) { contactRequest.resolve(); }
+        contactRequest = $q.defer();
+
         $scope.err = {};
+
         $http({
             method: 'POST',
             url: "contact.php",
             data: $scope.contact,
             headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
-        }).success(function(data) {
+        })
+        .success(function(data) {
             if(!data.success) {
                 $scope.err = data.err;
             }
             else {
                 $("#message").removeClass("alert error").addClass("alert success").slideDown("fast");
                 $scope.message = "Success, message has been sent!";
+                $timeout(function() {
+                    $("#message").slideUp("slow");
+                }, 2000);
             }
         });
     };
